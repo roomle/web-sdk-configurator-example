dce9c2235570bc764840c21de99d1903f2b8e150
Georg Kothmeier <georg.kothmeier@roomle.com>
2018-11-28 17:53:38 +0100

    [ Typo ] use correct enumeration  format

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 // 1. import the SDK
 import RoomleSdk from 'roomle-web-sdk';
-// 1a. import type definitions to use them to override default settings
+// 1.a import type definitions to use them to override default settings
 import {InitData} from 'roomle-web-sdk/lib/definitions/common-core/src/utils/shims';
-// 1b. import some function which can handle an update of parameters
+// 1.b import some function which can handle an update of parameters
 //     import some function which can handle an update of possible children
 //     import some function which can handle an update of the part list
 import {handleParameterUpdate, handlePossibleChildrenUpdate, handlePartlist} from './handler/events';
-// 1c. import UiKernelParameter, UiPossibleChild for type assistance
+// 1.c import UiKernelParameter, UiPossibleChild for type assistance
 import {UiKernelParameter, UiPossibleChild} from 'roomle-web-sdk/lib/definitions/typings/kernel';
 
 // 2. create an async iife to use async/await
@@ -18,33 +18,33 @@ import {UiKernelParameter, UiPossibleChild} from 'roomle-web-sdk/lib/definitions
     roomleConfigurator.boot();
 
     const options: InitData = {};
-    // 4a. enable hd rendering
+    // 4.a enable hd rendering
     options.enableHD = true;
-    // 4b. use different light setting
+    // 4.b use different light setting
     options.ls = 'shelf';
-    // 4c. override default settings
+    // 4.c override default settings
     roomleConfigurator.getApi().setOverrides(options);
 
     // 5. get the element where we want to append the canvas
     //    this is done with the browser native DOM function appendChild
     const element = document.getElementById('roomle-canvas-container');
 
-    // 5a. add some callback if the user changes a parameter
+    // 5.a add some callback if the user changes a parameter
     const setParameter = (parameter: UiKernelParameter, value: string) => {
-        // 5b. set the value of the parameter on the currently selected component
+        // 5.b set the value of the parameter on the currently selected component
         roomleConfigurator.getApi().setParameter(parameter, value);
     };
-    // 5c. register to events, depending on the events it makes sense to 
+    // 5.c register to events, depending on the events it makes sense to 
     //     add them before you init the canvas, e.g. hook into onUpdateParameters
     //     before init so you can receive the parameters on load
     roomleConfigurator.getApi().callbacks.onUpdateParameters = (parameters) => handleParameterUpdate(parameters, setParameter);
 
-    // 6a. add some callback if the user wants to dock something
+    // 6.a add some callback if the user wants to dock something
     const previewDockings = (possibleChild: UiPossibleChild, dragEvent: DragEvent) => {
-        // 6b. set the value of the parameter on the currently selected component
+        // 6.b set the value of the parameter on the currently selected component
         roomleConfigurator.getApi().previewDockings(possibleChild, dragEvent, (!!dragEvent) ? true : false);
     };
-    // 6c. register to events, depending on the events it makes sense to 
+    // 6.c register to events, depending on the events it makes sense to 
     //     add them before you init the canvas, e.g. hook into onUpdatePossibleChildren
     //     before init so you can receive the possible children on load
     roomleConfigurator.getApi().callbacks.onUpdatePossibleChildren = (tags) => handlePossibleChildrenUpdate(tags, previewDockings);
@@ -55,8 +55,6 @@ import {UiKernelParameter, UiPossibleChild} from 'roomle-web-sdk/lib/definitions
 
     // 8. initialize the canvas
     await roomleConfigurator.getApi().init(element);
-    // 8. load something (to know how to load see XXX section about conf vs item vs conf string)
-    await roomleConfigurator.getApi().loadConfigurableItemById('usm:frame');
     // 9.  load something (to know how to load see XXX section about conf vs item vs conf string)
     // 9.a fetch the ID from the query param id and load it correctly. Depending on the count of the :
     //     we either load a loadConfigurableItemById or loadConfigurationById
@@ -79,16 +77,16 @@ import {UiKernelParameter, UiPossibleChild} from 'roomle-web-sdk/lib/definitions
             break;
     }
 
-    // 9. register a click listener on a button which triggers the checkout process
+    // 10. register a click listener on a button which triggers the checkout process
     //    do this after init is finished because otherwise it could happen that you get invalid data
     document.querySelector('.checkout').addEventListener('click', async () => {
-        // 9.a fancy dom manipulation, should be done by your framework
+        // 10.a fancy dom manipulation, should be done by your framework
         document.querySelector('.modal-container').classList.add('show');
-        // 9.b save the current configuration to get the data. This also saves the configuration to
+        // 10.b save the current configuration to get the data. This also saves the configuration to
         //     the roomle furniture platform so it is always available if a user
         //     wants to restart from here
         const configuration = await roomleConfigurator.getApi().saveCurrentConfiguration();
-        // 9.c fancy dom manipulation, should be done by your framework
+        // 10.c fancy dom manipulation, should be done by your framework
         const img = document.createElement('img');
         img.src = configuration.perspectiveImage;
         const imageContainer = document.querySelector('.perspective-image');
@@ -97,6 +95,6 @@ import {UiKernelParameter, UiPossibleChild} from 'roomle-web-sdk/lib/definitions
         document.querySelector('.modal').classList.add('show');
     });
 
-    // 9.d fancy dom manipulation, should be done by your framework
+    // 10.d fancy dom manipulation, should be done by your framework
     document.querySelector('.modal__close').addEventListener('click', () => document.querySelector('.modal-container').classList.remove('show'));
 }()));
