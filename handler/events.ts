b4059af9b18e47432724e54b5a1c8689ac9f303d
Georg Kothmeier <georg.kothmeier@roomle.com>
2018-11-28 17:22:52 +0100

    [ Docu ] show how you could visualize the part list inside of a pop up

--- handler/events.ts
+++ handler/events.ts
@@ -1,4 +1,4 @@
-import {UiKernelParameter, UiPossibleChildTag, UiPossibleChild} from 'roomle-web-sdk/lib/definitions/typings/kernel';
+import {UiKernelParameter, UiPossibleChildTag, UiPossibleChild, KernelPartList, KernelPart} from 'roomle-web-sdk/lib/definitions/typings/kernel';
 import {handleParameter} from './parameters';
 import {handleAddonTag} from './addons';
 
@@ -16,4 +16,26 @@ export const handlePossibleChildrenUpdate = (tags: UiPossibleChildTag[], onPrevi
     addonsContainer.innerHTML = null;
     tags.forEach((tag) => addonsContainer.appendChild(handleAddonTag(tag, onPreviewDockings)));
     //paramters.forEach((parameter) => addonsContainer.appendChild(handleParameter(parameter, onSetValueOfParameter)));
+};
+
+export const handlePartlist = (fullList: KernelPart[], hash: string) => {
+    const container = document.querySelector('.partlist');
+    container.innerHTML = null;
+    fullList.forEach((part) => {
+        const row = document.createElement('tr');
+        const labelElement = document.createElement('td');
+        const idElement = document.createElement('td');
+        const countElement = document.createElement('td');
+        const priceElement = document.createElement('td');
+        const {componentId, label, count} = part;
+        labelElement.innerText = label;
+        idElement.innerText = componentId;
+        countElement.innerText = count.toFixed(0);
+        priceElement.innerText = (count * (Math.floor(Math.random() * (100 + 1)))).toFixed(2) + ' Fantasy Dollars';
+        row.appendChild(idElement);
+        row.appendChild(labelElement);
+        row.appendChild(countElement);
+        row.appendChild(priceElement);
+        container.appendChild(row);
+    });
 };
--- main.ts
+++ main.ts
@@ -4,7 +4,8 @@ import RoomleSdk from 'roomle-web-sdk';
 import {InitData} from 'roomle-web-sdk/lib/definitions/common-core/src/utils/shims';
 // 1b. import some function which can handle an update of parameters
 //     import some function which can handle an update of possible children
-import {handleParameterUpdate, handlePossibleChildrenUpdate} from './handler/events';
+//     import some function which can handle an update of the part list
+import {handleParameterUpdate, handlePossibleChildrenUpdate, handlePartlist} from './handler/events';
 // 1c. import UiKernelParameter, UiPossibleChild for type assistance
 import {UiKernelParameter, UiPossibleChild} from 'roomle-web-sdk/lib/definitions/typings/kernel';
 
@@ -48,8 +49,33 @@ import {UiKernelParameter, UiPossibleChild} from 'roomle-web-sdk/lib/definitions
     //     before init so you can receive the possible children on load
     roomleConfigurator.getApi().callbacks.onUpdatePossibleChildren = (tags) => handlePossibleChildrenUpdate(tags, previewDockings);
 
-    // 7. initialize the canvas
+    // 7. add a function which can handle updates to the part list also hook into this
+    //    function before you call init to get the inital part list as well
+    roomleConfigurator.getApi().callbacks.onPartListUpdate = handlePartlist;
+
+    // 8. initialize the canvas
     await roomleConfigurator.getApi().init(element);
     // 8. load something (to know how to load see XXX section about conf vs item vs conf string)
     await roomleConfigurator.getApi().loadConfigurableItemById('usm:frame');
+
+    // 9. register a click listener on a button which triggers the checkout process
+    //    do this after init is finished because otherwise it could happen that you get invalid data
+    document.querySelector('.checkout').addEventListener('click', async () => {
+        // 9.a fancy dom manipulation, should be done by your framework
+        document.querySelector('.modal-container').classList.add('show');
+        // 9.b save the current configuration to get the data. This also saves the configuration to
+        //     the roomle furniture platform so it is always available if a user
+        //     wants to restart from here
+        const configuration = await roomleConfigurator.getApi().saveCurrentConfiguration();
+        // 9.c fancy dom manipulation, should be done by your framework
+        const img = document.createElement('img');
+        img.src = configuration.perspectiveImage;
+        const imageContainer = document.querySelector('.perspective-image');
+        imageContainer.innerHTML = null;
+        imageContainer.appendChild(img);
+        document.querySelector('.modal').classList.add('show');
+    });
+
+    // 9.d fancy dom manipulation, should be done by your framework
+    document.querySelector('.modal__close').addEventListener('click', () => document.querySelector('.modal-container').classList.remove('show'));
 }()));
